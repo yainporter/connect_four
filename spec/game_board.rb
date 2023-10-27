a775ac5b40ec80f5fe187e35e85e3c3ca6bd61c9
Yain Porter <yainporter@gmail.com>
2023-10-26 19:42:47 -0700

    test: Write the #initialize and #welcome_message test for Game

---
+++ spec/game_board.rb
@@ -0,0 +1,25 @@
+require './lib/game'
+
+RSpec.describe Game do
+  describe '#initialize' do
+    it 'has @board' do
+      game = Game.new
+
+      expect(game.board).to eq ("ABCDEFG \n....... \n....... \n....... \n....... \n....... \n.......")
+    end
+  end
+
+  describe '#welcome_message' do
+    it 'prints out a welcome messsage' do
+      game = Game.new
+
+      expect(game.welcome_message).to eq("Hello!\n Welcome to Connect Four!")
+    end
+  end
+
+  describe '#start' do
+    it 'can start the game by printing out #welcome_message and @board' do
+      game = Game.new
+    end
+  end
+end
